e6a252db3648df7e986d24e79c82896090a03c8b
priscillamc <priscillamc@users.noreply.github.com>
2015-05-04 19:40:43 -0400

    Merges commits from CAIRES_theme
    About page template - Removes sidebar, text should be full-width now
    Revert About page template
    Under construction template - Adds footer
    Grants & teaching - Adds pagination and search function

--- p-teaching.php
+++ p-teaching.php
@@ -17,19 +17,63 @@ get_header(); ?>
     </div>
   </div>
 
+
+
+
 <div class="content-wrapper">
     <div class="row">
-        <div class="large-8 columns">
-          <?php if (have_posts()):
-              while (have_posts()) : the_post(); 
-                the_content();
-              endwhile;
-            endif; ?>
-        </div>
-        <div class="large-4 columns">
-          <?php dynamic_sidebar( 'teaching' ); ?>
-        </div>
+        <div class="large-12 columns">
 
-    </div>
+                <h3>Search by name or keyword</h3>
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>category/teaching-resources">
+  <label>
+    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+  </label>
+  <input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+</form>
 
+
+
+                    <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $args = array(
+      'cat' => '10',
+      'posts_per_page' => 10,
+      'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
+      'fields'  => 'ID',
+      'exclude' => array(
+        1
+      ),
+
+    );
+query_posts ($args); ?>
+
+<div class="pagination-centered">
+<ul class="text-center pagination">
+  <?php wpbeginner_numeric_posts_nav(); ?>
+</ul>
+</div>
+
+<?php while ( have_posts()) : the_post(); ?>
+
+                <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+                <span class="meta">
+                  <?php the_time('F jS, Y'); ?><br />
+                  <!--<?php the_author_link(); ?> -->
+                </span>
+                <div class="thumb left">
+                  <?php if ( has_post_thumbnail() ) : ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php the_post_thumbnail(); ?></a>
+                  <?php endif; ?>
+                </div>
+                <p><?php the_excerpt_max_charlength(280); ?>
+                <a href="<?php the_permalink(); ?>">Read more</a></p>
+                  <hr>
+                  <?php endwhile; ?>
+                    <?php wp_reset_query(); ?>
+
+          </div>
+  
+  </div>
+</div>
 <?php get_footer(); ?>
