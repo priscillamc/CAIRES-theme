307afd12cbcad9e97c56da5d578c9ff182c8e3f4
priscillamc <priscillamc@users.noreply.github.com>
2015-12-03 10:25:28 -0500

    Fix for the search on the Faculty Affiliates page

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# CAIRES-theme
+CAIRES-theme
+==============================
 
+Description
+------------
 WordPress theme for UF Center for Adaptive Innovation, Resilience, Ethics & Science using Foundation. 
+
+Installation
+------------
+Install and activate in a site.
+
+Changelog
+---------
+* [1.0.1] Fix for the search on the Faculty Affiliates page.
+* [1.0.0] Launch
+
+To-Do:
+------
+
+
+
+
--- p-facultyexperts.php
+++ p-facultyexperts.php
@@ -21,7 +21,7 @@ get_header(); ?>
     <div class="row">
         <div class="large-8 columns">
         <h3>Search by name or keyword</h3>
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>category/faculty-experts">
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>category/faculty-affiliates">
   <label>
     <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
   </label>
